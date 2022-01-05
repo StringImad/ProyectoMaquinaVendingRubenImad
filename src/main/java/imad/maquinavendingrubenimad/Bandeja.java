6488084502710dffd027e045cb98817d1b61c9af
imad <imadelfahssi@gmail.com>
2022-01-05 10:24:31 +0100

    Crea los  metodos

--- src/main/java/imad/maquinavendingrubenimad/Bandeja.java
+++ src/main/java/imad/maquinavendingrubenimad/Bandeja.java
@@ -15,5 +15,48 @@ public class Bandeja {
     private double precioProducto;
     private int stockBandeja;
 
-   
+    public Bandeja(String tipoDeProducto, int codigoProducto, double precioProducto, int stockBandeja) {
+        this.tipoDeProducto = tipoDeProducto;
+        this.codigoProducto = codigoProducto;
+        this.precioProducto = precioProducto;
+        this.stockBandeja = stockBandeja;
+    }
+
+    public String getTipoDeProducto() {
+        return tipoDeProducto;
+    }
+
+    public void setTipoDeProducto(String tipoDeProducto) {
+        this.tipoDeProducto = tipoDeProducto;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+
+    public int getStockBandeja() {
+        return stockBandeja;
+    }
+
+    public void setStockBandeja(int stockBandeja) {
+        this.stockBandeja = stockBandeja;
+    }
+
+    @Override
+    public String toString() {
+        return "Bandeja{" + "tipoDeProducto=" + tipoDeProducto + ", codigoProducto=" + codigoProducto + ", precioProducto=" + precioProducto + ", stockBandeja=" + stockBandeja + '}';
+    }
+    
 }
