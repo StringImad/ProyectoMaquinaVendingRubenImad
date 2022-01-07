5535557983620a5b879f70eaa1118f4be75f4061
imad <imadelfahssi@gmail.com>
2022-01-07 22:52:37 +0100

    Crea metodo para introducir cogdigo

--- src/main/java/imad/maquinavendingrubenimad/Programa.java
+++ src/main/java/imad/maquinavendingrubenimad/Programa.java
@@ -5,10 +5,53 @@
  */
 package imad.maquinavendingrubenimad;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author MSI
  */
 public class Programa {
-    
+
+    public static void main(String[] args) {
+        //Declaracion de variables
+        String codigo = "";
+        String codigoGenerado = "";
+        try {
+            codigo = JOptionPane.showInputDialog("Introduce un codigo");
+        } catch (NumberFormatException ex) {
+            //Mensaje de error
+            JOptionPane.showMessageDialog(null, "Formato incorrecto:\n"
+                    + "Por favor ingrese un valor valido", "Error de formato",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+        if (codigo.contentEquals(codigoGenerado)) {
+            JOptionPane.showMessageDialog(null, "Bienvenido al menu de administracion.");
+
+        } else {
+            switch (Integer.parseInt(codigo)) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "El precio es 1€.");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "El precio es 1€.");
+                    break;
+
+                case 3:
+                    JOptionPane.showMessageDialog(null, "El precio es 1€.");
+                    break;
+
+                case 4:
+                    JOptionPane.showMessageDialog(null, "El precio es 1€.");
+                    break;
+
+                case 5:
+                    JOptionPane.showMessageDialog(null, "El precio es 1€.");
+                    break;
+
+                case 6:
+
+            }
+        }
+    }
 }
