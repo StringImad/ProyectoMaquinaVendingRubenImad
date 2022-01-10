600f1416b260d32adc932294c3a34ce1f6e705e8
Rubenvs <rub3nvs97@gmail.com>
2022-01-10 10:43:02 +0100

    metodo para crear la contraseña para admin

--- src/main/java/imad/maquinavendingrubenimad/Utilidades.java
+++ src/main/java/imad/maquinavendingrubenimad/Utilidades.java
@@ -11,4 +11,39 @@ package imad.maquinavendingrubenimad;
  */
 public class Utilidades {
     
+    public String getPassword() {
+        
+        String numeros = "0123456789";
+
+        String mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        String minus = "abcdefghijklmnopqrstuvwxyz";
+
+        String esp = "#$%&()*+,-.:;<=>@";
+        
+        String todos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%&()*+,-.:;<=>@";
+
+        String pswd = "";
+
+        for (int i = 0; i < 1; i++) {
+            pswd += (numeros.charAt((int) (Math.random() * numeros.length())));
+        }
+
+        for (int i = 0; i < 1; i++) {
+            pswd += (mayus.charAt((int) (Math.random() * mayus.length())));
+        }
+        for (int i = 0; i < 1; i++) {
+            pswd += (minus.charAt((int) (Math.random() * minus.length())));
+        }
+        for (int i = 0; i < 1; i++) {
+            pswd += (esp.charAt((int) (Math.random() * esp.length())));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            pswd += (todos.charAt((int) (Math.random() * todos.length())));
+        }
+
+        return pswd;
+    }
+    
 }
