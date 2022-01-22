fdfe1200f38353e98362dec533851f4c5b9f0d5e
Rubenvs <rub3nvs97@gmail.com>
2022-01-22 15:01:13 +0100

    he cambiado el valor de fechaVencimiento por localDate que lo pide en el enunciado

--- src/main/java/imad/maquinavendingrubenimad/TarjetaDeCredito.java
+++ src/main/java/imad/maquinavendingrubenimad/TarjetaDeCredito.java
@@ -5,46 +5,45 @@
  */
 package imad.maquinavendingrubenimad;
 
+import java.time.LocalDate;
 
-import java.time.LocalDateTime;
 
 /**
  *
  * @author diabl
  */
 public class TarjetaDeCredito {
+
     private String numero;
-    private String fechaVencimiento;
+    private LocalDate fechaVencimiento;
     private int cvv;
 
-    public TarjetaDeCredito(String numero, String fechaVencimiento, int cvv) {
-        
-        this.numero = numero; 
+    public TarjetaDeCredito(String numero, LocalDate fechaVencimiento, int cvv) {
+        this.numero = numero;
         this.fechaVencimiento = fechaVencimiento;
         this.cvv = cvv;
     }
-    
+
     //Método para saber si la tarjeta es valida para realizar el pago 
-//    public static boolean ValidarFecha(LocalDateTime fecha){
-//        LocalDateTime ahora = LocalDateTime.now();
-//        return fecha.isBefore(ahora);
-//    }
-    
+    public boolean ValidarFecha(LocalDate fecha){
+        LocalDate ahora = LocalDate.now();
+        return fecha.isAfter(ahora);
+    }
     public boolean CompararTarjetas(TarjetaDeCredito t1) {
-        
-    return t1.cvv == this.cvv && t1.numero == this.numero && t1.fechaVencimiento == this.fechaVencimiento; 
-}
-    
+
+        return t1.cvv == this.cvv && t1.numero == this.numero && t1.fechaVencimiento == this.fechaVencimiento;
+    }
+
     public String getNumero() {
         return numero;
     }
 
-    public String getFechaVencimiento() {
+    public LocalDate getFechaVencimiento() {
         return fechaVencimiento;
     }
 
     public int getCvv() {
         return cvv;
     }
-    
+
 }
